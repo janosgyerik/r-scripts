aa8f4225e5670eaae5f2a6aca9a9c959c4c90abf
Janos Gyerik <janos@kronos>
2014-12-14 19:13:54 +0100

    made the method more readable

--- scripts/find.col.R
+++ scripts/find.col.R
@@ -2,10 +2,14 @@ find.col <- function(df, pattern, ...) {
   count.pattern <- function(name) {
     length(grep(pattern, df[,name], ...))
   }
-  work <- subset(data.frame(name=names(df), count=sapply(names(df), count.pattern)), count > 0)
-  rownames(work) <- NULL
-  work
+  name <- names(df)
+  count <- sapply(name, count.pattern)
+  name.with.count <- data.frame(name, count)
+  rownames(name.with.count) <- NULL
+  name.with.count.nonzero <- subset(name.with.count, count > 0)
+  name.with.count.nonzero
 }
 # Example:
 # find.col(iris, 'set')
+# find.col(iris, 'set|vers')
 # find.col(iris, 'Set', ignore.case = T)
